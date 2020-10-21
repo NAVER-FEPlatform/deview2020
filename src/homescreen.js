6881af32197b24d53865e9dddad0f9603948fb0f
daybrush <daybrush@gmail.com>
2020-10-21 17:26:34 +0900

    feat: add homescreen

--- src/homescreen.js
+++ src/homescreen.js
@@ -1,1 +1,13 @@
 import "./css/homescreen.css";
+import Flicking from "@egjs/flicking";
+
+const flicking = new Flicking(".homescreen .container");
+const dots = document.querySelectorAll(".dot");
+
+flicking.on("change", e => {
+  dots.forEach(dot => {
+    dot.classList.remove("active");
+  });
+
+  dots[e.index].classList.add("active");
+});
